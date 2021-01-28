b26cb997295e26c24e26a9779bf643b953000c57
brahim-kellou <fb_kellou@esi.dz>
2021-01-28 20:36:27 +0100

    fix: add middleware cors

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 from fastapi import Request, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config import PDGLINT_MODEL_ENDPOINT
 from services import get_stats
 
@@ -9,6 +10,21 @@ url = PDGLINT_MODEL_ENDPOINT
 headers = {'Content-type': 'application/json'}
 
 app = FastAPI()
+
+origins = [
+    "http://pdglint.azurewebsites.net",
+    "https://pdglint.azurewebsites.net",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello_pdglint():
